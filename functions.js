53cc8f859f436fd6692da44a6225d54219f2e0dc
fma350 <francesco.moneta@gmail.com>
2018-11-22 10:24:57 +0000

    Js functions are now in their own file

---
+++ functions.js
@@ -0,0 +1,16 @@
+function load_comments() {
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    print("your browser is too old.")
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("comments").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","server.php?q=load_comments",true);
+  xmlhttp.send();
+}
